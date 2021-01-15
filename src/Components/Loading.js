49ca689a9dca78ab04f7988f0932387be66d690a
umang <25umangagarwal@gmail.com>
2021-01-15 23:51:51 +0530

    Made spinner reesizable

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import spinner from '../Assets/spinner.gif'
-
+import styled from 'styled-components'
+const SpinnerComp = styled.div`
+    height:${props => props.height ? props.height: "5em"};
+    width:${props => props.width ? props.width: "5em"};
+    img{
+        height:100%;
+        width:100%;
+        }
+`
 function Loading() {
     return (
         <>
+        <SpinnerComp>
         <img src={spinner} alt=""/>
+        </SpinnerComp>
+        
             
         </>
     )
